17cc7ec88143632d14b98cdd706cd578454338aa
Rogerio Chaves <rogeriocfj@gmail.com>
2020-03-26 18:13:49 +0100

    Save to json

--- app/process_csv.R
+++ app/process_csv.R
@@ -4,6 +4,7 @@
 #install.packages("incidence")
 #install.packages("projections")
 #install.packages("distcrete")
+#install.packages("rjson")
 
 library(here)
 library(ggplot2)
@@ -11,41 +12,58 @@ library(magrittr)
 library(incidence)
 library(projections)
 library(distcrete)
+library(rjson)
 
 simulate_cases <- readRDS(here("rds", "simulate_cases.rds"))
 
 scripts <- list.files("scripts", pattern = "projections[.]R$", full.names = TRUE)
 lapply(scripts, source)
 
-casos <- read.csv(here("caso.csv"))
-deaths_rj <- casos[(casos$state == "RJ" & casos$deaths > 0 & casos$place_type == "state"),]
-deaths_rj <- deaths_rj[order(deaths_rj[,1]),]
+states_with_death <- casos[(casos$deaths > 0 & casos$place_type == "state"),"state"]
+states_with_death <- unique(states_with_death)
 
-dates <- c()
-total_deaths <- 0
-for (i in 1:nrow(deaths_rj)) {
-  new_deaths <- deaths_rj[i, "deaths"] - total_deaths
-  total_deaths <- total_deaths + new_deaths
-  date_deaths <- rep(paste(deaths_rj[i, "date"]), new_deaths)
-  dates <- c(dates, date_deaths)
+estimates <- data.frame("RJ",0,0,0,0, stringsAsFactors=FALSE)
+names(estimates) <- c("state", "1%", "5%", "95%", "99%")
+
+for (state in states_with_death) {
+  casos <- read.csv(here("caso.csv"))
+  deaths_state <- casos[(casos$state == state & casos$deaths > 0 & casos$place_type == "state"),]
+  deaths_state <- deaths_state[order(deaths_state[,1]),]
+  
+  dates <- c()
+  total_deaths <- 0
+  for (i in 1:nrow(deaths_state)) {
+    new_deaths <- deaths_state[i, "deaths"] - total_deaths
+    total_deaths <- total_deaths + new_deaths
+    date_deaths <- rep(paste(deaths_state[i, "date"]), new_deaths)
+    dates <- c(dates, date_deaths)
+  }
+  
+  clean_date <- as.Date(unlist(dates))
+  clean_date <- clean_date[!is.na(clean_date)]
+  clean_date <- clean_date[order(clean_date)]
+  date_range <- clean_date[length(clean_date)] - clean_date[1]
+  date_death = clean_date
+  
+  result <- simulate_cases(date_death,
+                 R = 2,
+                 cfr = 0.02,
+                 duration = 1,
+                 n_sim = 100)
+  
+  projections <- result$projections
+  projections_df <- as.data.frame(projections)
+  last_row <- projections_df[nrow(projections_df) - 1,]
+  x <- unlist(tail(as.list(last_row), -1))
+  
+  quantiles <- quantile(x, probs = c(0.01, 0.05, 0.95, 0.99))
+  estimates[nrow(estimates) + 1,] = c(state, quantiles[1], quantiles[2], quantiles[3], quantiles[4])
 }
 
-clean_date <- as.Date(unlist(dates))
-clean_date <- clean_date[!is.na(clean_date)]
-clean_date <- clean_date[order(clean_date)]
-date_range <- clean_date[length(clean_date)] - clean_date[1]
-date_death = clean_date
+estimates = estimates[-1,]
+estimates
 
-result <- simulate_cases(date_death,
-               R = 2,
-               cfr = 0.02,
-               duration = 1,
-               n_sim = 100)
+json <- toJSON(unname(split(estimates, 1:nrow(estimates))))
+json
 
-projections <- result$projections
-projections_df <- as.data.frame(projections)
-last_row <- projections_df[nrow(projections_df) - 1,]
-x <- unlist(tail(as.list(last_row), -1))
-
-quantile(x, probs = c(0.01, 0.05, 0.95, 0.99))
-
+write(json, file = "result.json")
